ca98e27522292dd13eb28d3dde88c9077139e9ed
Meaghan <meaghan.sharrard89@gmail.com>
2023-12-12 13:36:25 -0800

    passing tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,25 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self._word = word
+
+    @property
+    def word(self):
+        """The word property"""
+        return self._word
+
+    @word.setter
+    def word(self, word):
+        self._word = word
+
+    def match(self, options):
+        """Return a list of words from the options"""
+        list = []
+        for word in options:
+            if self.is_anagram(word):
+                list.append(word)
+        return list
+
+    def is_anagram(self, option):
+        """Check if option is an anagram of the initial word"""
+        return sorted(self.word.lower()) == sorted(option.lower())
